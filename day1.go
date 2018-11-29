7fcf5685c6fc1d56a23dc0e181d0473dd5e92b3d
kidoda <kdevb0x@gmail.com>
2018-11-29 11:03:30 -0700

    moved a global into main()

--- day1.go
+++ day1.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
-var link string = "https://adventofcode.com/2017/day/1/input"
-var data = getDataFromHTTP(link, nil)
+var link = "https://adventofcode.com/2017/day/1/input"
 
 func main() {
-
+	data, err := getDataFromHTTP(link, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(data)
 }
--- utility.go
+++ utility.go
@@ -10,48 +10,48 @@ import (
 	"os"
 )
 
-func getDataFromHTTP(link string, to ...io.Writer) error {
+func getDataFromHTTP(link string, to ...io.Writer) ([]byte, error) {
 	var tmpbuf bytes.Buffer
 	cwd, _ := os.Getwd()
 	file, err := os.Create(cwd + link + ".txt")
 	if err != nil {
 		log.Printf("failed to create file %s, error: %s \n", link, err)
-		return err
+		return nil, err
 	}
 	defer file.Close()
 
 	resp, err := http.Get(link)
 	if err != nil {
 		log.Println(err)
-		return err
+		return nil, err
 	}
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
-		return err
+		return nil, err
 	}
 
 	if read, _ := tmpbuf.Write(data); read != len(data) {
 		copyerr := fmt.Errorf("ineffectual copy to buffer; copied: %d out of %d bytes", read, len(data))
 		log.Print(copyerr)
 
-		return copyerr
+		return nil, copyerr
 	}
 
 	for i := 0; i < len(to); i++ {
 		_, err := to[i].Write(tmpbuf.Bytes())
 		if err != nil {
 			log.Println(err)
-			return err
+			return nil, err
 		}
 
 	}
 
 	if _, err := file.Write(tmpbuf.Bytes()); err != nil {
 		log.Println(err)
-		return err
+		return nil, err
 	}
 	fmt.Printf("data from HTTP request written to file: %s", cwd+link+".txt")
-	return nil
+	return tmpbuf.Bytes(), nil
 }
